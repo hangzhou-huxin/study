adf967204d8f1c722d3a3a686fe732291ec03b14
hangzhou-huxin <huxinstudy@163.com>
2015-09-23 17:44:18 +0800

    创建项目,初次导入

---
+++ src/main/java/org/huxin/study/shiro/MyRealm.java
@@ -0,0 +1,26 @@
+package org.huxin.study.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm implements Realm{
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean supports(AuthenticationToken token) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token)
+			throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
